843d426778a9f5e428f6c68bb894c365260db57a
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-09-29 19:17:17 +0200

    Create the Delivery About section

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,9 +1,11 @@
+import DeliveryAbout from "@/components/DeliveryAbout/DeliveryAbout";
 import HeroAbout from "@/components/HeroAbout/HeroAbout";
 
 function AboutPage() {
   return (
     <section>
       <HeroAbout />
+      <DeliveryAbout />
     </section>
   );
 }
