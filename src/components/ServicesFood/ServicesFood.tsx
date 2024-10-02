83127418bbeec6ad2b6a29a764e10441a1872d7c
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-10-02 13:02:57 +0200

    Make the product dynamic

--- src/components/ServicesFood/ServicesFood.tsx
+++ src/components/ServicesFood/ServicesFood.tsx
@@ -1,274 +1,50 @@
 import "./ServicesFood.scss";
 import Image from "next/image";
+import { foodItems } from "@/constants/index";
 
 function ServicesFood() {
   return (
     <section className="services_food">
       <div className="services_food__container">
-        <div className="services_food__item">
-          <div className="services_food__item__image">
-            <Image
-              src="/images/food/burger.jpg"
-              alt="Burger"
-              width={200}
-              height={150}
-            />
-            <div className="services_food__item__body">
-              <div className="services_food__item__body__top">
-                <h3 className="services_food__item__title">Burger</h3>
-                <h5 className="services_food__item__category">Hamburger</h5>
-              </div>
-              <div className="services_food__item__body__bottom">
-                <div className="services_food__item__body__bottom__stars">
-                  <div className="services_food__item__body__bottom__stars__icon">
-                    <Image
-                      src="/images/icons/star.png"
-                      alt="Star"
-                      width={20}
-                      height={20}
-                    />
+        {foodItems.map((item) => (
+          <div className="services_food__item" key={item.id}>
+            <div className="services_food__item__image">
+              <Image
+                src={item.image}
+                alt={item.title}
+                width={200}
+                height={150}
+              />
+              <div className="services_food__item__body">
+                <div className="services_food__item__body__top">
+                  <h3 className="services_food__item__title">{item.title}</h3>
+                  <h5 className="services_food__item__category">
+                    {item.category}
+                  </h5>
+                </div>
+                <div className="services_food__item__body__bottom">
+                  <div className="services_food__item__body__bottom__stars">
+                    <div className="services_food__item__body__bottom__stars__icon">
+                      <Image
+                        src="/images/icons/star.png"
+                        alt="Star"
+                        width={20}
+                        height={20}
+                      />
+                    </div>
+                    <p>{item.rating}</p>
                   </div>
-                  <p>4.5</p>
+                  <div className="services_food__item__body__bottom__price">
+                    <p>${item.price}</p>
+                  </div>
                 </div>
-                <div className="services_food__item__body__bottom__price">
-                  <p>$10</p>
-                </div>
+                <button className="services_food__item__button">
+                  Order Now
+                </button>
               </div>
-              <button className="services_food__item__button">Order Now</button>
             </div>
           </div>
-        </div>
-        <div className="services_food__item">
-          <div className="services_food__item__image">
-            <Image
-              src="/images/food/meat.jpg"
-              alt="Burger"
-              width={200}
-              height={150}
-            />
-            <div className="services_food__item__body">
-              <div className="services_food__item__body__top">
-                <h3 className="services_food__item__title">Meat</h3>
-                <h5 className="services_food__item__category">Meat</h5>
-              </div>
-              <div className="services_food__item__body__bottom">
-                <div className="services_food__item__body__bottom__stars">
-                  <div className="services_food__item__body__bottom__stars__icon">
-                    <Image
-                      src="/images/icons/star.png"
-                      alt="Star"
-                      width={20}
-                      height={20}
-                    />
-                  </div>
-                  <p>4.5</p>
-                </div>
-                <div className="services_food__item__body__bottom__price">
-                  <p>$10</p>
-                </div>
-              </div>
-              <button className="services_food__item__button">Order Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="services_food__item">
-          <div className="services_food__item__image">
-            <Image
-              src="/images/food/noodles.jpg"
-              alt="Burger"
-              width={200}
-              height={150}
-            />
-            <div className="services_food__item__body">
-              <div className="services_food__item__body__top">
-                <h3 className="services_food__item__title">Noodles</h3>
-                <h5 className="services_food__item__category">Noodles</h5>
-              </div>
-              <div className="services_food__item__body__bottom">
-                <div className="services_food__item__body__bottom__stars">
-                  <div className="services_food__item__body__bottom__stars__icon">
-                    <Image
-                      src="/images/icons/star.png"
-                      alt="Star"
-                      width={20}
-                      height={20}
-                    />
-                  </div>
-                  <p>4.5</p>
-                </div>
-                <div className="services_food__item__body__bottom__price">
-                  <p>$10</p>
-                </div>
-              </div>
-              <button className="services_food__item__button">Order Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="services_food__item">
-          <div className="services_food__item__image">
-            <Image
-              src="/images/food/salad.jpg"
-              alt="Burger"
-              width={200}
-              height={150}
-            />
-            <div className="services_food__item__body">
-              <div className="services_food__item__body__top">
-                <h3 className="services_food__item__title">Salad</h3>
-                <h5 className="services_food__item__category">Salad</h5>
-              </div>
-              <div className="services_food__item__body__bottom">
-                <div className="services_food__item__body__bottom__stars">
-                  <div className="services_food__item__body__bottom__stars__icon">
-                    <Image
-                      src="/images/icons/star.png"
-                      alt="Star"
-                      width={20}
-                      height={20}
-                    />
-                  </div>
-                  <p>4.5</p>
-                </div>
-                <div className="services_food__item__body__bottom__price">
-                  <p>$10</p>
-                </div>
-              </div>
-              <button className="services_food__item__button">Order Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="services_food__item">
-          <div className="services_food__item__image">
-            <Image
-              src="/images/food/pizza.jpg"
-              alt="Burger"
-              width={200}
-              height={150}
-            />
-            <div className="services_food__item__body">
-              <div className="services_food__item__body__top">
-                <h3 className="services_food__item__title">Pizza</h3>
-                <h5 className="services_food__item__category">Pizza</h5>
-              </div>
-              <div className="services_food__item__body__bottom">
-                <div className="services_food__item__body__bottom__stars">
-                  <div className="services_food__item__body__bottom__stars__icon">
-                    <Image
-                      src="/images/icons/star.png"
-                      alt="Star"
-                      width={20}
-                      height={20}
-                    />
-                  </div>
-                  <p>4.5</p>
-                </div>
-                <div className="services_food__item__body__bottom__price">
-                  <p>$10</p>
-                </div>
-              </div>
-              <button className="services_food__item__button">Order Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="services_food__item">
-          <div className="services_food__item__image">
-            <Image
-              src="/images/food/mexican.jpg"
-              alt="Burger"
-              width={200}
-              height={150}
-            />
-            <div className="services_food__item__body">
-              <div className="services_food__item__body__top">
-                <h3 className="services_food__item__title">Mexican</h3>
-                <h5 className="services_food__item__category">Mexican</h5>
-              </div>
-              <div className="services_food__item__body__bottom">
-                <div className="services_food__item__body__bottom__stars">
-                  <div className="services_food__item__body__bottom__stars__icon">
-                    <Image
-                      src="/images/icons/star.png"
-                      alt="Star"
-                      width={20}
-                      height={20}
-                    />
-                  </div>
-                  <p>4.5</p>
-                </div>
-                <div className="services_food__item__body__bottom__price">
-                  <p>$10</p>
-                </div>
-              </div>
-              <button className="services_food__item__button">Order Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="services_food__item">
-          <div className="services_food__item__image">
-            <Image
-              src="/images/food/fish.jpg"
-              alt="Burger"
-              width={200}
-              height={150}
-            />
-            <div className="services_food__item__body">
-              <div className="services_food__item__body__top">
-                <h3 className="services_food__item__title">Fish</h3>
-                <h5 className="services_food__item__category">Fish</h5>
-              </div>
-              <div className="services_food__item__body__bottom">
-                <div className="services_food__item__body__bottom__stars">
-                  <div className="services_food__item__body__bottom__stars__icon">
-                    <Image
-                      src="/images/icons/star.png"
-                      alt="Star"
-                      width={20}
-                      height={20}
-                    />
-                  </div>
-                  <p>4.5</p>
-                </div>
-                <div className="services_food__item__body__bottom__price">
-                  <p>$10</p>
-                </div>
-              </div>
-              <button className="services_food__item__button">Order Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="services_food__item">
-          <div className="services_food__item__image">
-            <Image
-              src="/images/food/chicken.jpg"
-              alt="Burger"
-              width={200}
-              height={150}
-            />
-            <div className="services_food__item__body">
-              <div className="services_food__item__body__top">
-                <h3 className="services_food__item__title">Chicken</h3>
-                <h5 className="services_food__item__category">Chicken</h5>
-              </div>
-              <div className="services_food__item__body__bottom">
-                <div className="services_food__item__body__bottom__stars">
-                  <div className="services_food__item__body__bottom__stars__icon">
-                    <Image
-                      src="/images/icons/star.png"
-                      alt="Star"
-                      width={20}
-                      height={20}
-                    />
-                  </div>
-                  <p>4.5</p>
-                </div>
-                <div className="services_food__item__body__bottom__price">
-                  <p>$10</p>
-                </div>
-              </div>
-              <button className="services_food__item__button">Order Now</button>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
