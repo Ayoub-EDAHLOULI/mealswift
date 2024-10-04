821a185f9d0bfa67adef087d1ea812eb24a53623
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-10-04 10:16:02 +0200

    Create the Driver dashboard

---
+++ src/components/DriverDashboard/(Tabs)/DashboardTab/DashboardTab.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./DashboardTab.scss";
+
+function DashboardTab() {
+  return <div>DashboardTab</div>;
+}
+
+export default DashboardTab;
--- src/components/DriverDashboard/DriverDashboard.tsx
+++ src/components/DriverDashboard/DriverDashboard.tsx
@@ -1,8 +1,54 @@
+"use client";
+
 import React from "react";
+import { useState } from "react";
 import "./DriverDashboard.scss";
+import DashboardTab from "./(Tabs)/DashboardTab/DashboardTab";
+import ShortBackground from "../ShortBackground/ShortBackground";
 
 function DriverDashboard() {
-  return <div>DriverDashboard</div>;
+  const [activeTab, setActiveTab] = useState<string>("Dashboard");
+
+  // Function to handle tab change
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab);
+  };
+
+  // Function to render the content based on the active tab
+  const renderContent = () => {
+    switch (activeTab) {
+      case "Dashboard":
+        return <DashboardTab />;
+      default:
+        return <DashboardTab />;
+    }
+  };
+
+  return (
+    <>
+      <ShortBackground
+        title="Delivery Driver Dashboard"
+        background="/images/hero_delivery.jpg"
+      />
+      <section className="driver-dashboard">
+        <div className="driver-dashboard__container">
+          <aside className="driver-dashboard__sidebar">
+            <div className="driver-dashboard__sidebar__items">
+              <ul>
+                <li
+                  onClick={() => handleTabChange("Dashboard")}
+                  className={activeTab === "Dashboard" ? "active" : ""}
+                >
+                  Dashboard
+                </li>
+              </ul>
+            </div>
+          </aside>
+          <main className="driver-dashboard__content">{renderContent()}</main>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default DriverDashboard;
